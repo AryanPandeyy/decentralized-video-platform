eb2dfc482bbd54436acbf5904a25e0ff6c9c4374
Aryan Pandey <margiaryan@gmail.com>
2023-10-25 08:35:50 +0530

    truffle init

---
+++ app/video/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import playlist from "@/build/contracts/playlist.json";
+import { useEffect } from "react";
+import Web3 from "web3";
+
+export default function Video() {
+  const web3 = new Web3(window.ethereum);
+  console.log("ETH PROVIDER ", web3.eth);
+  const init = async (): Promise<void> => {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    const accounts = await web3.eth.getAccounts();
+    const id = await web3.eth.net.getId();
+    console.log("ID ", id);
+    console.log("Account Address ", accounts);
+    const playListFunciton = new web3.eth.Contract(
+      playlist.abi,
+      accounts[0]
+    );
+    const result = await playListFunciton.methods.myFunction().call();
+    console.log("RESULT ", result);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl">Decentralized Video Sharing Platform</h1>
+    </main>
+  );
+}
