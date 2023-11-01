addd144d33babd290825c0cec86aacbb680ff45e
Aryan Pandey <margiaryan@gmail.com>
2023-11-01 19:26:57 +0530

    feat: added ipfs

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import playlist from "@/build/contracts/PlayList.json";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { create } from "kubo-rpc-client";
 import Web3 from "web3";
 
 export default function Video() {
+  const [selectedFile, setSelectedFile] = useState<File>();
   const web3 = new Web3(window.ethereum);
   console.log("ETH PROVIDER ", web3.eth);
+  const ipfsClient = create(new URL("http://127.0.0.1:5001"));
   const init = async (): Promise<void> => {
     await window.ethereum.request({ method: "eth_requestAccounts" });
     const accounts = await web3.eth.getAccounts();
@@ -20,6 +23,22 @@ export default function Video() {
     console.log("RESULT ", result);
   };
 
+  const handleUpload = async (e) => {
+    e.preventDefault();
+    try {
+      // const { path, cid, size } = await ipfsClient.add("test");
+      // console.log(path);
+      const result = ipfsClient.cat(
+        "QmRf22bZar3WKmojipms22PkXH1MZGmvsqzQtuSvQE3uhm",
+      );
+      for await (const num of result) {
+        console.log(num)
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   useEffect(() => {
     init();
   }, []);
@@ -27,6 +46,13 @@ export default function Video() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl">Decentralized Video Sharing Platform</h1>
+      <form onSubmit={handleUpload}>
+        <input
+          type="file"
+          onChange={(e) => setSelectedFile(e.target.files[0])}
+        />
+        <button type="submit">Upload</button>
+      </form>
     </main>
   );
 }
