65db5d04d84d158bd8da58754c97e9a748b382af
Aryan Pandey <margiaryan@gmail.com>
2024-02-10 15:32:45 +0530

    video contract

--- contracts/PlayList.sol
+++ contracts/PlayList.sol
@@ -1,5 +1,58 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >= 0.4.22 < 0.9.0;
+pragma experimental ABIEncoderV2;
+
 contract PlayList {
-  function myFunction() public pure returns(bool) { return true; }
+  struct video {
+    uint id;
+    address creator;
+    string desc;
+    string title;
+    string thumbHash;
+    string videoHash;
+  } video[] public videoArray;
+  function addVideo(string memory _thumbHash, string memory _videoHash,
+                    string memory _title,
+                    string memory _desc) public returns(bool) {
+    if (isExisting(_thumbHash, _videoHash)) {
+      return false;
+    }
+    uint _id = videoArray.length;
+    videoArray.push(video({
+      id : _id,
+      title : _title,
+      thumbHash : _thumbHash,
+      videoHash : _videoHash,
+      desc : _desc,
+      creator : msg.sender
+    }));
+    return true;
+  }
+  function isExisting(string memory _thumbHash,
+                      string memory _videoHash) public view returns(bool) {
+    for (uint i = 0; i < videoArray.length; i++) {
+      if (keccak256(abi.encodePacked(videoArray[i].thumbHash)) ==
+              keccak256(abi.encodePacked(_thumbHash)) ||
+          keccak256(abi.encodePacked(videoArray[i].videoHash)) ==
+              keccak256(abi.encodePacked(_videoHash))) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function getVideo(uint _id) public view returns(string memory) {
+    return string(
+        abi.encodePacked(videoArray[_id].title, "/", videoArray[_id].thumbHash,
+                         "/", videoArray[_id].videoHash, "/",
+                         videoArray[_id].desc, "/", videoArray[_id].creator));
+  }
+
+  function getVideos() public view returns(video[] memory) {
+    return videoArray;
+  }
+
+  function getArrayLength() public view returns(uint) {
+    return videoArray.length;
+  }
 }
