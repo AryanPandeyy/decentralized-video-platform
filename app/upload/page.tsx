3fce1372236ff6b45ced9e49bb8ef59961ed48e2
Aryan Pandey <margiaryan@gmail.com>
2024-02-14 06:57:34 +0530

    refactoring

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,21 +1,36 @@
 "use client";
-import Web3 from "web3";
 import { IPFSHTTPClient, create } from "kubo-rpc-client";
 import { useState, FormEvent, useEffect, useRef } from "react";
-import Playlist from "@/build/contracts/PlayList.json";
+import { isUser } from "@/utils/isUser";
+import { loadBlockChainData } from "@/utils/web3";
+import { Contract, ContractAbi } from "web3";
+
+interface ConType {
+  accounts: string[];
+  networkID: bigint;
+  contract: Contract<ContractAbi>;
+  Playlist: Object;
+}
+
 export default function Home() {
-  const [selectedVideoFile, setSelectedVideoFile] = useState();
-  const [selectedImageFile, setSelectedImageFile] = useState();
-  const [title, setTitle] = useState<String>();
-  const [desc, setDesc] = useState<String>();
+  const [selectedVideoFile, setSelectedVideoFile] = useState<File>();
+  const [selectedImageFile, setSelectedImageFile] = useState<File>();
+  const [thumbHash, setThumbHash] = useState<string>("");
+  const [web3, setWeb3] = useState<ConType>();
+  const [videoHash, setVideoHash] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+  const [desc, setDesc] = useState<string>("");
   const [ipfs, setIpfs] = useState<IPFSHTTPClient>();
+
   const videoRef = useRef<HTMLVideoElement>();
   const imageRef = useRef();
+  const user = isUser();
 
   useEffect(() => {
     const init = async () => {
-      // const client = create(new URL("http://127.0.0.1:5001"));
       const client = create({ url: "http://127.0.0.1:5001" });
+      const con: ConType = await loadBlockChainData();
+      setWeb3(con);
       setIpfs(client);
     };
     init();
@@ -23,24 +38,15 @@ export default function Home() {
 
   async function handleSubmit(e: FormEvent<HTMLFormElement>): Promise<void> {
     e.preventDefault();
-    const web3 = new Web3(window.ethereum);
-    const tmp = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-    console.log(tmp);
-    // https://stackoverflow.com/questions/73838680/unable-to-call-function-from-smart-contract-using-web3-js
-    const project = new web3.eth.Contract(
-      Playlist.abi,
-      Playlist.networks[5777].address,
-    );
-
     if (!ipfs) return;
+
     const reader = new window.FileReader();
     if (!selectedVideoFile) return;
     reader.readAsArrayBuffer(selectedVideoFile);
     reader.onloadend = async () => {
       const { path } = await ipfs.add(reader.result as ArrayBuffer);
       console.log(path);
+      setVideoHash(path);
       const site = `https://ipfs.io/ipfs/${path}/`;
       console.log(site);
       if (videoRef.current) {
@@ -49,65 +55,83 @@ export default function Home() {
     };
 
     const imgReader = new window.FileReader();
-    if (!selectedImageFile) {
-      console.log("not selected image");
-    } else {
-      imgReader.readAsArrayBuffer(selectedImageFile);
-      imgReader.onloadend = async () => {
-        const { path } = await ipfs.add(imgReader.result as ArrayBuffer);
-        console.log(path);
-        const site = `https://ipfs.io/ipfs/${path}/`;
-        console.log(site);
-        if (imageRef.current) {
-          imageRef.current.src = site;
-        }
-      };
-    }
-    const retailer = await project.methods.isExisting();
+    if (!selectedImageFile) return;
+    imgReader.readAsArrayBuffer(selectedImageFile);
+    imgReader.onloadend = async () => {
+      const { path } = await ipfs.add(imgReader.result as ArrayBuffer);
+      console.log(path);
+      setThumbHash(path);
+      const site = `https://ipfs.io/ipfs/${path}/`;
+      console.log(site);
+      if (imageRef.current) {
+        imageRef.current.src = site;
+      }
+    };
+    console.log("log ", await web3?.contract.methods.getVideos().call());
   }
 
+  const handleWeb3 = async (
+    thumbHash: string,
+    videoHash: string,
+    title: string,
+    desc: string,
+  ) => {
+    console.log(thumbHash, videoHash, title, desc);
+    if (thumbHash && videoHash && title && desc) {
+      await web3?.contract.methods
+        .addVideo(thumbHash, videoHash, title, desc)
+        .send({ from: `${user}` });
+    }
+    console.log("log ", await web3?.contract.methods.getVideos().call());
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <img ref={imageRef}></img>
-      <video ref={videoRef} controls></video>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="file"
-          placeholder="Video File"
-          onChange={(e) => {
-            if (e.target.files) {
-              setSelectedVideoFile(e.target.files[0]);
-            }
-          }}
-        />
-        <br />
-        <input
-          type="file"
-          placeholder="Video Image"
-          onChange={(e) => {
-            if (e.target.files) {
-              setSelectedImageFile(e.target.files[0]);
-            }
-          }}
-        />
-        <br />
-        <input
-          value={title}
-          placeholder="Video Title"
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-        />
-        <br />
-        <input
-          value={desc}
-          placeholder="Video Description"
-          onChange={(e) => {
-            setDesc(e.target.value);
-          }}
-        />
-        <button type="submit">Upload Your Video</button>
-      </form>
-    </main>
+    <div className="container flex flex-row gap-4 justify-center item-center border border-black p-20">
+      <div>
+        <img ref={imageRef}></img>
+        <video ref={videoRef} controls></video>
+      </div>
+      <div className="container flex flex-col gap-4 m-20">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="file"
+            placeholder="Video File"
+            onChange={(e) => {
+              if (e.target.files) {
+                setSelectedVideoFile(e.target.files[0]);
+              }
+            }}
+          />
+          <br />
+          <input
+            type="file"
+            placeholder="Video Image"
+            onChange={(e) => {
+              if (e.target.files) {
+                setSelectedImageFile(e.target.files[0]);
+              }
+            }}
+          />
+          <br />
+          <input
+            value={title}
+            placeholder="Video Title"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+          <br />
+          <input
+            value={desc}
+            placeholder="Video Description"
+            onChange={(e) => {
+              setDesc(e.target.value);
+            }}
+          />
+          <br />
+          <button type="submit">Upload Your Video</button>
+        </form>
+      </div>
+    </div>
   );
 }
--- utils/web3.ts
+++ utils/web3.ts
@@ -1,12 +1,19 @@
 import Web3 from "web3";
+import Playlist from "@/build/contracts/PlayList.json";
 
+// https://stackoverflow.com/questions/73838680/unable-to-call-function-from-smart-contract-using-web3-js
 export const loadBlockChainData = async () => {
   try {
     const web3 = new Web3(window.ethereum);
     await window.ethereum.request({ method: "eth_requestAccounts" });
     const accounts = await web3.eth.getAccounts();
     const networkID = await web3.eth.net.getId();
-    return { accounts, networkID };
+    const signer = web3.eth.accounts.sign;
+    const contract = new web3.eth.Contract(
+      Playlist.abi,
+      Playlist.networks[5777].address,
+    );
+    return { accounts, networkID, contract, Playlist };
   } catch (e) {
     console.log("ERROR: ", e);
   }
